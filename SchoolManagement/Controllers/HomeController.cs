55492574573ba2d2dd593e2b02b21a19f35e5712
geoken <geoken90@gmail.com>
2017-11-27 00:08:35 +0200

    Add project files.

---
+++ SchoolManagement/Controllers/HomeController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace SchoolManagement.Controllers
+{
+    [RoutePrefix("Home")]
+    public class HomeController : Controller
+    {
+        [Route("~/")]
+        [HttpGet]
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        [Route("About")]
+        [HttpGet]
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            return View();
+        }
+
+        [Route("Contact")]
+        [HttpGet]
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
